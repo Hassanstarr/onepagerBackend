32ae2a608cf722f5bdef828b29a7ac50c8cb3b40
HassanStarr <hassanasif419@gmail.com>
2026-09-16 17:59:11 +0500

    Configure CORS to allow requests from specified frontend URL

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import contactRouter from "./src/routes/contact.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
+}));
 app.use(express.json());
 
 connectDB();
